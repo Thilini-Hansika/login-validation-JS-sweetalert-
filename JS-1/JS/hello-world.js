5a1616dd635daf0d1d8bf1d15dc8440d35481794
unknown <hansigunawardhana100@gmail.com>
2018-11-20 18:58:22 +0530

    Login  validation using java script and

---
+++ JS-1/JS/hello-world.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Thilini Hansika on 6/17/2018.
+ */
+console.log("Hello World");
+
+var name="Ijse";
+var address="Galle";
+
+console.log(name);
+console.log(address);
+
+var myArray =[1,2,3,4,5];
+console.log(myArray);
+
+for(var index in myArray){
+    console.log(myArray[index]);
+}
+function myMethod() {
+    console.log("Method111")
+
+
+}
